20a8a009ea6682c89e3262a66b3a57c38e9da6d6
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-01-16 14:46:24 -0300

    fazendo as "function"
    vou ter que da uma parada PC ta sem espaço...

--- velha.js
+++ velha.js
@@ -1,11 +1,24 @@
 // document.body.classList.toggle("") || document.getElementById("btnSortear").disabled = true;
     
+
+let testeB = Number(0)
+let testeX = Number(0)
+
+let opa = document.getElementById("")
+
 function bola(){
     document.getElementById("xis").disabled = true
 
-        
+        testeB = 1
 }
 function xis(){
     document.getElementById("bola").disabled = true
 
+    testex = 1
+}
+
+function bolaORxis() {
+    if (testeB || testeX == 0) {
+
+    }
 }
