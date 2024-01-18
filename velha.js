116b228c07b070db03d5e3bd3cd759c056a5de45
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-01-18 17:47:30 -0300

    CSS
    estou tentando add uma class nos button mas não esta dando certo, mais tarde vejo isso.

--- velha.js
+++ velha.js
@@ -1,22 +1,40 @@
-// document.body.classList.toggle("") || document.getElementById("btnSortear").disabled = true;
+/*  document.body.classList.toggle("")
     
+    document.getElementById("btnSortear").disabled = true;
+
+    const elemento = document.querySelector('.meu-elemento');
+    elemento.className += ' minha-classe';   
+
+
+*/
 
 let testeB = Number(0)
 let testeX = Number(0)
 
 // let opa = document.getElementById("")
 
+
+// funçoes que qando chamadas liberam um parametro(1) de aprovação.
 function bola(){
-    document.getElementById("xis").disabled = true
+    document.querySelector('.xis').disabled = true
 
         testeB = 1
+
+        const novoX = document.querySelector('.xis');
+            novoX.classList.toggle('outraX');
+        
 }
 function xis(){
     document.getElementById("bola").disabled = true
 
-    testex = 1
+    testeX = 1
+
+    const novoB = document.querySelector('.bola');
+            novoB.classList.add('outraB');
 }
 
+/* função que verifica os parametros. 
+ (agora e fazer ela chama outra função para continuar o jogo) */ 
 function bolaORxis() {
     if (testeB == 0 && testeX == 0) {
 
