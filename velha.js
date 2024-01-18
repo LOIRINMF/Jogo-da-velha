dcd174b73d8b02136bad8bacfe85e5ec8936f6cf
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-01-18 18:39:52 -0300

    Alguns ajustes..

--- velha.js
+++ velha.js
@@ -8,6 +8,7 @@
 
 */
 
+// parametros de verificação
 let testeB = Number(0)
 let testeX = Number(0)
 
@@ -16,25 +17,34 @@ let testeX = Number(0)
 
 // funçoes que qando chamadas liberam um parametro(1) de aprovação.
 function bola(){
-    document.querySelector('.xis').disabled = true
+    document.querySelector('.btXis').disabled = true
 
         testeB = 1
 
-        const novoX = document.querySelector('.xis');
+        const novoX = document.querySelector('.btXis');
             novoX.classList.toggle('outraX');
-        
+
+            for(let i = 1; i < 10; i++){
+
+            let caixas = document.querySelector('#l5')
+                caixas.classList.add('certo')
+        }    
 }
 function xis(){
     document.getElementById("bola").disabled = true
 
     testeX = 1
 
-    const novoB = document.querySelector('.bola');
+    const novoB = document.querySelector('.btBola');
             novoB.classList.add('outraB');
+
+            const caixas = document.querySelector('#l6')
+                caixas.classList.add('certo')
 }
 
 /* função que verifica os parametros. 
  (agora e fazer ela chama outra função para continuar o jogo) */ 
+
 function bolaORxis() {
     if (testeB == 0 && testeX == 0) {
 
