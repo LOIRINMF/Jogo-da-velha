0b2906d8d2a009755cf41483aa6b75f88bbf2b76
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-01-20 15:55:05 -0300

    function verde()
    ainda não consegui fazer um loop que faz isso, então por agora e isso.

--- velha.js
+++ velha.js
@@ -16,34 +16,59 @@ let testeX = Number(0)
 
 
 // funçoes que qando chamadas liberam um parametro(1) de aprovação.
-function bola(){
+function bola() {
     document.querySelector('.btXis').disabled = true
 
-        testeB = 1
+    testeB = 1
 
-        const novoX = document.querySelector('.btXis');
-            novoX.classList.toggle('outraX');
+    const novoX = document.querySelector('.btXis');
+    novoX.classList.toggle('outraX');
 
-            for(let i = 1; i < 10; i++){
-
-            let caixas = document.querySelector('#l5')
-                caixas.classList.add('certo')
-        }    
+    verde()
 }
-function xis(){
+function xis() {
     document.getElementById("bola").disabled = true
 
     testeX = 1
 
     const novoB = document.querySelector('.btBola');
-            novoB.classList.add('outraB');
+    novoB.classList.add('outraB');
 
-            const caixas = document.querySelector('#l6')
-                caixas.classList.add('certo')
+    // Deixar as div box verde.
+    verde()
+}
+
+function verde() {
+    let c1 = document.querySelector('#l1')
+    c1.classList.add('certo')
+
+    let c2 = document.querySelector('#l2')
+    c2.classList.add('certo')
+
+    let c3 = document.querySelector('#l3')
+    c3.classList.add('certo')
+
+    let c4 = document.querySelector('#l4')
+    c4.classList.add('certo')
+
+    let c5 = document.querySelector('#l5')
+    c5.classList.add('certo')
+
+    let c6 = document.querySelector('#l6')
+    c6.classList.add('certo')
+
+    let c7 = document.querySelector('#l7')
+    c7.classList.add('certo')
+
+    let c8 = document.querySelector('#l8')
+    c8.classList.add('certo')
+
+    let c9 = document.querySelector('#l9')
+    c9.classList.add('certo')
 }
 
 /* função que verifica os parametros. 
- (agora e fazer ela chama outra função para continuar o jogo) */ 
+ (agora e fazer ela chama outra função para continuar o jogo) */
 
 function bolaORxis() {
     if (testeB == 0 && testeX == 0) {
