f202512a1b8b80cdaf5dfa50e98aab85d345ca7b
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-02-08 12:20:09 -0300

    Update velha.js

--- public/JS/velha.js
+++ public/JS/velha.js
@@ -4,20 +4,14 @@
 
     const elemento = document.querySelector('.meu-elemento');
     elemento.className += ' minha-classe';   
-
-
 */
 
 // parametros de verificação
 let testeB = Number(0)
 let testeX = Number(0)
 
-// let opa = document.getElementById("")
-
-
-// funçoes que qando chamadas liberam um parametro(1) de aprovação.
-
-// PENSEI EM UMA FORMA DE RESUMIR A LILHA 19 A 40 PASANDO UM PARAMETRO PRA FUNCION
+// funçoes que quando chamadas liberam um parametro(1) de aprovação.
+// PENSEI EM UMA FORMA DE RESUMIR A LILHA 15 A 35 PASANDO UM PARAMETRO PRA FUNCION
 function bola() {
     document.querySelector('.btXis').disabled = true
     document.getElementById("bola").disabled = true
@@ -60,14 +54,12 @@ function verde() {
  (agora e fazer ela chama outra função para continuar o jogo) */
 
 function bolaORxis(a) {
+    let erro = document.getElementById("opa")
     if (testeB == 0 && testeX == 0) {
+        erro.innerHTML = "Selecione com qual letra você ira começa X ou BOLA!!"
 
-        let erro = document.getElementById("opa")
-
-        erro.innerHTML = "Selecione com qual letra você ira começa primeiro!!"
     } else {
         // Se tive algo escrito aqui ele apaga.
-        let erro = document.getElementById("opa")
         erro.innerHTML = ""
 
         //Pegando o ID da box e colocando em uma (var)
@@ -87,19 +79,9 @@ function bolaORxis(a) {
         }
 
         L.onclick = null;
+        jogar(a)
     }
 }
-
-// funcion do chatGPT (O cara e bom!!) 
-let jogadorAtual = 'X';
-
-function jogar(element) {
-    if (element.innerHTML === '') {
-        element.innerHTML = jogadorAtual;
-        jogadorAtual = jogadorAtual === 'X' ? 'O' : 'X';
-    }
-}
-
 
 // codigo de ferificaçao do GPT
 let tabuleiroV = [
@@ -109,21 +91,21 @@ let tabuleiroV = [
 ];
 
 function verificarVencedor() {
-    // Verificar linhas
+    // Verificar L
     for (let i = 0; i < 3; i++) {
         if (tabuleiroV[i][0] !== "" && tabuleiroV[i][0] === tabuleiroV[i][1] && tabuleiroV[i][1] === tabuleiroV[i][2]) {
             return tabuleiroV[i][0];
         }
     }
 
-    // Verificar colunas
+    // Verificar C
     for (let j = 0; j < 3; j++) {
         if (tabuleiroV[0][j] !== "" && tabuleiroV[0][j] === tabuleiroV[1][j] && tabuleiroV[1][j] === tabuleiroV[2][j]) {
             return tabuleiroV[0][j];
         }
     }
 
-    // Verificar diagonais
+    // Verificar D
     if (tabuleiroV[0][0] !== "" && tabuleiroV[0][0] === tabuleiroV[1][1] && tabuleiroV[1][1] === tabuleiroV[2][2]) {
         return tabuleiroV[0][0];
     }
@@ -132,21 +114,22 @@ function verificarVencedor() {
         return tabuleiroV[0][2];
     }
 
-    return null;  // Nenhum vencedor
+    return null;
 }
 
-// Atualizar a matriz do tabuleiro com a jogada
-    // Supondo que "a" seja um índice válido entre 1 e 9
+function jogar(a) {
     let linha = Math.floor((a - 1) / 3);
     let coluna = (a - 1) % 3;
-    tabuleiroV[linha][coluna] = testeB === 1 ? "O" : "X";
+    
+    tabuleiroV[linha][coluna] = testeB === 1 ? "X" : "O";
 
     // Verificar se há um vencedor após a jogada
     let vencedor = verificarVencedor();
     if (vencedor !== null) {
         alert("Jogador " + vencedor + " venceu!");
-        // Lógica para reiniciar o jogo ou qualquer outra ação desejada
+        // Lógica para reiniciar o jogo.
     }
+}
 
 //Animação
 // Simulando o restante do código sendo exibido após 7 segundos
