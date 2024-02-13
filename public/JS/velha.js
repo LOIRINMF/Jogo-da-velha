ee43f18622572acfb90e35dc750ba9ee2fa3adfd
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-02-13 18:55:10 -0300

    Comentando o codigo.

--- public/JS/velha.js
+++ public/JS/velha.js
@@ -6,7 +6,7 @@
     elemento.className += ' minha-classe';   
 */
 
-// parametros de verificação
+// Parametros de verificação
 let testeB = Number(0)
 let testeX = Number(0)
 
@@ -18,7 +18,6 @@ function bola() {
     document.querySelector('.btXis').disabled = true
     document.getElementById("bola").disabled = true
     testeB = 1
-
 
     // Deixar as div box verde.
     verde()
@@ -52,20 +51,18 @@ function verde() {
     );
 }
 
-/* função que verifica os parametros. 
- (agora e fazer ela chama outra função para continuar o jogo) */
-
+/* função que verifica os parametros. */
 function bolaORxis(a) {
     if (testeB == 0 && testeX == 0) {
         sistema.innerHTML = "Selecione com qual letra você ira começa X ou BOLA!!"
-console.log('if')
+
     } else {
 
         //Pegando o ID da box e colocando em uma (var)
         const H = document.querySelector('#bOx' + a)
         const L = document.querySelector('#l' + a)
         let S = ''
-console.log('else')
+
         // fazer a separaçao de bola ou xis ||CERTO||
         if (testeB == 1) {
             H.innerHTML = "O"
@@ -87,13 +84,14 @@ console.log('else')
     }
 }
 
-// codigo de ferificaçao do GPT
+// Tabuleiro virtual
 let tabuleiroV = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""]
 ];
 
+// Verificar ser alguem venceu.
 function verificarVencedor() {
     // Verificar L
     for (let i = 0; i < 3; i++) {
@@ -121,46 +119,52 @@ function verificarVencedor() {
     return null;
 }
 
+//Passa os paramtros pro tabuleiro virtual e chama o verificador de vencedor.
 function jogar(a) {
     let linha = Math.floor((a - 1) / 3);
     let coluna = (a - 1) % 3;
     tabuleiroV[linha][coluna] = testeB === 1 ? "X" : "O";
-    
+
     // Verificar se há um vencedor após a jogada
     let vencedor = verificarVencedor();
     console.log('teste1000')
     if (vencedor !== null) {
-        
+
         sistema.innerHTML = ("Parabens Jogador " + vencedor + " você venceu!");
-        
+
         // Lógica para reiniciar o jogo.   
         limp()
     }
 }
+
+// Ser ouver vencedor apos mostra quem foi essa função reseta o jogo, (pronto para outra rodada?)
 function limp() {
 
+    // Limpa os <h3>
     for (let L = 1; L < 10; L++) {
-
         let Limpar = document.querySelector('#bOx' + L);
 
         Limpar.innerHTML = ""
 
     }
-    
+    // Limpa o tabuleiro virtual
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             tabuleiroV[i][j] = '';
         }
     }
 
+    // Devolve a função onclick das div.box
     let boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
         box.onclick = () => bolaORxis(parseInt(box.id.substr(1)));
     });
 
+    // Reseta os parametros de verifição
     testeB = Number(0)
     testeX = Number(0)
 
+    // Libera os botoês para joga denovo
     document.querySelector('.btXis').disabled = false
     document.getElementById("bola").disabled = false
 }
